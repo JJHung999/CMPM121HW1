1a72650bfdf590814175744f8d32c83f49532a35
JJHung999 <65639615+JJHung999@users.noreply.github.com>
2022-10-07 18:19:29 -0700

    Altered some of the scripts for the movement
    Altered some of the scripts for the movement

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -11,15 +11,25 @@ public class MovementScript : MonoBehaviour
     }
 
     //movement speed in units per second
-    private float movementSpeed = 15f;
+    private float movementSpeed = 180f;
 
     // Update is called once per frame
     void Update()
     {
+        //Set the old position
+        float ogposition = new Vector3 (0.0f,0.0f,0.0f);
         //get the Input from Horizontal axis (arrow keys)
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = 0;
+        //Input.GetAxis("Horizontal")
         //get the Input from Vertical axis
-        float verticalInput = Input.GetAxis("Vertical");
+        float verticalInput = Time.deltaTime;
+        //Input.GetAxis("Vertical")
+
+        //If object travels a certain distance, teleport back to original position
+        if transform.position(horizontalInput * movementSpeed * Time.deltaTime, 0, 222)
+        {
+            transform.position = ogposition;
+        }
 
         //update the position
         transform.position = transform.position + new Vector3(horizontalInput
@@ -27,6 +37,12 @@ public class MovementScript : MonoBehaviour
                             verticalInput * movementSpeed * Time.deltaTime);
 
         //output to log the position change
-        // Debug.Log(transform.position);
+        Debug.Log(transform.position);
+    }
+
+    //Teleport the object back to it's original position
+    void teleport()
+    {
+
     }
 }
