09f013b59df0c5b3f62c1935c890167938bdae20
JJHung999 <65639615+JJHung999@users.noreply.github.com>
2022-10-28 17:32:00 -0700

    Made the rotate script work

--- Assets/rotate.cs
+++ Assets/rotate.cs
@@ -10,21 +10,11 @@ public class rotate : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    float smooth = 5.0f;
-    float tiltAngle = 60.0f;
+    public float xAngle, yAngle, zAngle;
 
     void Update()
     {
-        // Smoothly tilts a transform towards a target rotation.
-        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
-        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
-
-        // Rotate the cube by converting the angles into a quaternion.
-        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
-
-        // Dampen towards the target rotation
-        transform.rotation = Quaternion.Slerp(transform.rotation, target,  Time.deltaTime * smooth);
+        transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
 
     }
 }
